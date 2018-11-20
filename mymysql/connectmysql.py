29938b07dcfcf0f5ef9c8263646f582765b9f2cf
WaitInRainging <1759139860@qq.com>
2018-11-20 16:47:46 +0800

    modify

--- mymysql/connectmysql.py
+++ mymysql/connectmysql.py
@@ -20,18 +20,18 @@ except mysql.connector.Error as e:
 cursor = conn.cursor()
 
 try:
-    query_movies = 'select MovieID from movies'
-    cursor.execute(query_movies)
-    #data = cursor.fetchall()
-    # data = []
-    movies=[]
-    for movieid in cursor:
-        # line=[movieid,title,genres.split('|')]
-        # data.append(line)
-        movies.append(movieid)
+    # query_movies = 'select * from movies'
+    # cursor.execute(query_movies)
+    # #data = cursor.fetchall()
+    # # data = []
+    # movies=[]
+    # for movieid in cursor:
+    #     # line=[movieid,title,genres.split('|')]
+    #     # data.append(line)
+    #     movies.append(movieid)
     # print len(data)
 
-    query_users = 'select  UserID from users'
+    query_users = 'select  UserID, Gender, Age, Occupation, Zip-code from users'
     cursor.execute(query_users)
     users=[]
     for userID in cursor:
@@ -42,20 +42,20 @@ except mysql.connector.Error as e:
 
 # print movies[-1][0], users[-1][0]
 # 初始化用户-物品矩阵
-dataMat = zeros((users[-1][0],movies[-1][0]))
+# dataMat = zeros((users[-1][0],movies[-1][0]))
 
 try:
     query_ratings = "select UserID, MovieID,Rating from ratings"
     cursor.execute(query_ratings)
     for userid, movieid, rating in cursor:
         print userid-1, movieid-1, rating
-        dataMat[userid-1, movieid-1]=rating
+        # dataMat[userid-1, movieid-1]=rating
 
 finally:
     cursor.close()
     conn.close()
 #得到的是一个list,(编号， 评分)
-print Similarity.userSimiliar(mat(dataMat), 1, Similarity.cosSim)
+# print Similarity.userSimiliar(mat(dataMat), 1, Similarity.cosSim)
 
 
 
